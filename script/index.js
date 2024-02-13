5b30cebe0d1956901d04a93af08c564a42d6bdb2
MILON-2377 <153436745+MILON-2377@users.noreply.github.com>
2024-02-13 21:08:55 +0800

    to remove the to do list item javascript added

--- script/index.js
+++ script/index.js
@@ -2,3 +2,48 @@
 const inputBox = document.getElementById("input-field");
 const displayItem = document.getElementById("display-text");
 const button = document.getElementById("button");
+const listContainer = document.getElementById("list-container");
+
+button.addEventListener("click", function(){
+    const inputValue = inputBox.value;
+
+    if(inputValue !== ""){
+         // create li 
+        const li = document.createElement("li");
+        const divContainer = document.createElement("div");
+        divContainer.classList.add("check-box");
+        divContainer.innerHTML = `<i id="check-icon" class="fa-solid fa-check"></i>`;
+        li.appendChild(divContainer);
+
+
+        const span = document.createElement("span");
+        span.classList.add("display");
+        span.innerText = inputValue;
+        li.appendChild(span);
+
+
+        const p = document.createElement("p");
+        p.classList.add("remove-box")
+        p.innerText = "x";
+        li.appendChild(p)
+
+
+        // to remove the element from the li list
+
+        li.classList.add("list-item")
+        listContainer.appendChild(li);
+        inputBox.value = "";
+    }else{
+        alert("please type a text!")
+    }
+   
+}, false)
+
+const listBox = document.querySelectorAll(".ul-box");
+
+for(const item of listBox){
+    item.addEventListener("click", function(e){
+        const parent = e.target.parentNode;
+        parent.remove();
+    })
+}
